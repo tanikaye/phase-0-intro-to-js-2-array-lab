ce3547282707c91b1b7a3dcfea6a7fb3107ffa52
Tani Kaye <1tanikaye@gmail.com>
2021-09-23 12:39:54 -0400

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+
+  var cats = ["Milo", "Otis", "Garfield"];
+
+  function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+
+  function appendCat(name) {
+    var sliced = cats.slice()
+    sliced.push(name)
+    return sliced
+  }
+
+  function prependCat(name) {
+    var sliced = cats.slice()
+    sliced.unshift(name)
+    return sliced
+  }
+
+  function removeLastCat() {
+    var sliced = cats.slice()
+    sliced.pop()
+    return sliced
+  }
+
+  function removeFirstCat() {
+    var sliced = cats.slice()
+    sliced.shift()
+    return sliced
+  }
